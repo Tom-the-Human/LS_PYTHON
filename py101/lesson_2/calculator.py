18e895f00449c36787dbbeb64d11f4629561cc0a
Tom Hodges <tomhodges83@gmail.com>
2024-08-02 08:39:28 -0400

    Calculator localization functioning, still need to fix result formatting

--- py101/lesson_2/calculator.py
+++ py101/lesson_2/calculator.py
@@ -4,21 +4,32 @@ calculator.py
 I followed the directions mostly, but took opportunities 
 to experiment and try variations on the suggested code. 
 In the end, much of code is what is suggested, but there 
-are differences. Most notably I decided to use floats rather 
-than ints because I think a calculator that can only handle 
-whole numbers lacks utility. 
+are differences. 
 
-Function docstrings only added to satisfy Pylint.
-Why does Pylint think the variable 'result' is meant to be a constant?
+Function docstrings added to satisfy Pylint.
+Why does Pylint think the variable 'result' is meant to be a constant? 
+Found a code to disable the invalid-name flag.
 """
 
-def prompt(message):
+import json
+
+with open('calculator.json', 'r') as file:
+    OUTPUT = json.load(file)
+
+def messages(message, lang='es'):
+    """
+    Accesses and returns output message data.
+    """
+    return OUTPUT[lang][message]
+
+def prompt(output_key):
     """
     Output formatter.
     """
+    message = messages(output_key, LANGUAGE)
     print(f"=> {message} <=")
 
-def invalid_number(number_str):
+def invalid_num(number_str):
     """
     Checks if number is valid.
     """
@@ -29,47 +40,59 @@ def invalid_number(number_str):
 
     return False
 
-prompt('Welcome to Calculator!')
-prompt("What's the first number?")
-num1 = input()
+LANGUAGE = 'es'
 
-while invalid_number(num1):
-    prompt("Hmm ... that doesn't look right. Please enter a valid number.")
+while True:
+    prompt('welcome')
+    prompt('get_first_num')
     num1 = input()
 
-num1 = float(num1)
+    while invalid_num(num1):
+        prompt('invalid_num')
+        num1 = input()
 
-prompt("What's the second number?")
-num2 = input()
+    num1 = float(num1)
 
-while invalid_number(num2):
-    prompt("Hmm ... that doesn't look right. Please enter a valid number.")
+    prompt('get_second_num')
     num2 = input()
 
-num2 = float(num2)
+    while invalid_num(num2):
+        prompt('invalid_num')
+        num2 = input()
 
-prompt("Enter '+' to add, '-' to subtract, '*' to multiply, or '/' to divide.")
-operator = input()
+    num2 = float(num2)
 
-while operator not in ['+', '-', '*', '/']:
-    prompt('Please choose a valid operator: +, -, *, or /!')
+    prompt('get_operator')
     operator = input()
 
-match operator:
-    case '+':
-        result = num1 + num2
-    case '-':
-        result = num1 - num2
-    case '*':
-        result = num1 * num2
-    case '/':
-        if num2 == 0.0:
-            try:
-                result = num1 / num2
-            except ZeroDivisionError:
-                result = None
-                prompt("Cannot divide by zero!")
-        else:
-            result = num1 / num2
+    while operator not in ['+', '-', '*', '/']:
+        prompt('invalid_operator')
+        operator = input()
 
-prompt(f'Result: {result}')
+    match operator:
+        case '+':
+            result = num1 + num2
+        case '-':
+            result = num1 - num2
+        case '*':
+            result = num1 * num2
+        case '/':
+            if num2 == 0.0:
+                try:
+                    result = num1 / num2
+                except ZeroDivisionError:
+                    result = None #pylint: disable=C0103
+                    prompt('zero_div')
+            else:
+                result = num1 / num2
+
+#can't get fstring to work without assigning at runtime FIX THIS
+    OUTPUT['en']['result'] = f'Result: {result}' 
+    OUTPUT['es']['result'] = f'El resultado: {result}'
+    prompt('result') 
+
+    prompt('another_calc')
+    restart = input()
+    if restart and restart[0].lower() != 'y':
+        prompt('goodbye')
+        break
