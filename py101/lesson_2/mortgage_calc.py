6386280962111f10877045f350e8f50bbd12e921
Tom Hodges <tomhodges83@gmail.com>
2024-08-11 18:23:40 -0400

    mortgage_calc 1.0 w support for US & UK currency

--- py101/lesson_2/mortgage_calc.py
+++ py101/lesson_2/mortgage_calc.py
@@ -1,31 +1,22 @@
-# GET loan amount, APR, term
-# calculate and SET monthly interest rate (APR/12), loan term in months
-# monthly_payment = loan_value * (monthly_rate / (1 - (1 + monthly_rate) ** (-term_months)))
-# print payment amount as dollars and cents ($xx.xx)
-# use Pylint
-# consider UX (enter 5% interest as '5' rather than '.05')
-# name varaibles carefully, i.e. monthly vs. yearly
-# edge cases - no-interest loans, non-integer payment periods, 
-# negative numbers, other nonsense inputs
-
 """
-Reusing parts of my calculator program. Should work well here.
-
+Simple loan calculator. Reusing parts of my calculator program. Should work well here.
 """
 
-import os
 import json
+import locale
+import os
 
 with open('mortgage_messages.json', 'r', encoding='utf-8') as file:
     OUTPUT = json.load(file)
 
-LOCALE = 'en_US.UTF-8'
+LOCALE = locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+CURRENCY_SYMBOL = locale.localeconv()['currency_symbol']
 
-def messages(message, locale):
+def messages(message, locale):                  # pylint: disable=w0621
     """
     Accesses and returns output message data.
     """
-    if message not in OUTPUT[locale]:         # pylint: disable=R1705
+    if message not in OUTPUT[locale]:           # pylint: disable=R1705
         return message      # formatted result must call messages directly
     else:
         return OUTPUT[locale][message]
@@ -48,7 +39,7 @@ def is_valid_amount(number_str):
         float(number_str)
     except ValueError:
         return False
-    
+
     if float(number_str) <= 0:
         return False
 
@@ -65,7 +56,7 @@ def is_valid_apr(number_str):
         float(number_str)
     except ValueError:
         return False
-    
+
     if float(number_str) < 0:
         return False
 
@@ -77,37 +68,48 @@ def is_valid_term(number_str):
     """
     if number_str in ('nan', 'inf', 'infinity'):
         return False
-    
+
     try:
         int(number_str)
     except ValueError:
         return False
-    
+
     if int(number_str) <= 0:
         return False
 
     return True
 
-def calc_monthly_payment(loan_value, monthly_rate, term_months):
+def calc_monthly_payment(loan_amount, monthly_rate, months):
     """
     Calculates monthly payment.
     """
     if monthly_rate == 0:
-        return loan_value / term_months
-    else:
-        return loan_value * ((monthly_rate / 100) / (1 - (1 + monthly_rate) ** (-term_months)))
+        return loan_amount / months
+
+    return loan_amount * ((monthly_rate) / (1 - (1 + monthly_rate) ** (-months)))
+
+def print_results():
+    """
+    Prints results to terminal.
+    """
+    prompt(f'Loan amount: {CURRENCY_SYMBOL}{loan_value:.2f}')
+    prompt(f'APR: {apr}%')
+    prompt(f'Loan term: {term_months} months')
+    prompt(messages('result', LOCALE).format(monthly_payment=monthly_payment))
+
 
 while True:
 
     os.system('clear')
 
+    prompt('welcome')
     prompt('get_loan_amount')
-    loan_value = input(messages('currency_symbol', LOCALE))
+    loan_value = input(CURRENCY_SYMBOL)
     loan_value = loan_value.replace('$', '').replace('£', '').replace(',', '')
 
-    while is_valid_amount(loan_value) == False:
-        prompt('invalid_num') 
-        loan_value = input(messages('currency_symbol', LOCALE))
+    while is_valid_amount(loan_value) is False:
+        prompt('invalid_num')
+        loan_value = input(CURRENCY_SYMBOL)
         loan_value = loan_value.replace('$', '').replace('£', '').replace(',', '')
 
 
@@ -116,25 +118,26 @@ while True:
     prompt('get_APR')
     apr = input().replace('%', '').replace(',', '')
 
-    while is_valid_apr(apr) == False: 
+    while is_valid_apr(apr) is False:
         prompt('invalid_num')
         apr = input()
 
     apr = float(apr)
-    monthly_rate = apr / 12
+    monthly_interest = apr / 100 / 12
 
     prompt('get_term')
     term_months = input()
 
-    while is_valid_term(term_months) == False:
+    while is_valid_term(term_months) is False:
         prompt('invalid_term')
         term_months = input()
 
     term_months = int(term_months)
 
-    monthly_payment = round(calc_monthly_payment(loan_value, monthly_rate, term_months), 2)
-    prompt(messages('result', LOCALE).format(monthly_payment=monthly_payment))
-    # prompt('Monthly payment placeholder.')
+    monthly_payment = round(calc_monthly_payment(loan_value, monthly_interest, term_months), 2)
+
+    print_results()
+
     prompt('again?')
     restart = input()
     if restart and restart[0].lower() not in ('y', 's'):
