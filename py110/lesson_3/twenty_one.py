f5af2208f36ad2825a7a6ce961512de28f0462b5
Tom-the-Human <tomhodges83@gmail.com>
2024-11-03 10:00:32 -0500

    chips axed, otherwise done for now

--- py110/lesson_3/twenty_one.py
+++ py110/lesson_3/twenty_one.py
@@ -1,109 +1,9 @@
-# P
-# 1 Init deck
-# 2 Deal cards to player + dealer
-# 3 Player turn - hit or stay
-#   repeat until bust or stay
-# 4 If player bust, dealer wins
-# 5 Dealer turn - hit or stay
-#   repeat until total >= 17
-# 6 if dealer bust, player wins
-# 7 compare cards and declare winner
-
-# Examples
-# Possible play representation
-# Dealer : +-----+ +-----+
-#          |K    | |:::::|  Showing: 10
-#          |♠️   ♠️| |:::::|
-#          |    K| |:::::|  Dealer Stays
-#          +-----+ +-----+
-#
-#
-# Player:  +-----+ +-----+
-#          |9    | |A    |
-#          |  ♦️  | |  ♣️  |
-#          |    9| |    A|
-#          +-----+ +-----+
-#
-#           Total: 20
-#           Player Stays
-#
-# This could require some fancy formatting. Maybe I store the names of the
-# cards as dict keys with the ASCII art as the values, i.e.
-# ... actually deck = {d_9: [9, ♦️]} could work well if I follow the 
-# formatting in the Tic Tac Toe game, and do something like 
-# def display_card(deck[card]):
-#   print('+----+')
-#   print(f'|{deck[card][0]}    |')
-#   print(f'|  {deck[card][1]}  |')
-#   print(f'|    {deck[card][0]}|')
-#   print('+----+')
-#
-# Seems like it could be tricky to figure out how to get it all to display
-# correctly in the terminal. Needs to be modular for 2 cards up to 5 or more.
-# Will need to use dicts to rep the player and dealer hands, and then can
-# print cards * length of dict i.e. 
-# edge = '+-----+ '
-# card_top = f'|{deck[card][0]}    | '
-# card_mid = f'|  {deck[card][1]}  | '
-# card_low = f'|    {deck[card][0]}| '
-# --card = f'{edge}\ncard--
-# print(edge * len(hand))
-# print(card_top for card in hand)
-# print(card_mid for card in hand)
-# print(card_bot for card in hand)
-# print(edge * len(hand))
-#
-# Maybe that is all contained in a display_hand function.
-#
-# Getting the total of a hand can be along the lines of 
-# total += hand[card][0] for cards in hand, or hopefully just sum(hand).
-#
-# Oooh, maybe instead of ASCII lines I can use codes for white backgrounds
-# black or red text on cards as befitting the suit.
-
-# D
-# I was pretty set on using dicts for the deck and hands with list values for 
-# the card values, but I see LS recommends nested lists. Maybe that's better -
-# Since cards are drawn randomly, we really have no need of keys. Actually,
-# maybe a set of lists for the deck is even better since the deck is meant to
-# be randomly shuffled anyway. So a set of lists for the deck and lists of
-# lists for the hands (since hands should retain order).
-
-# A
-# 1 Welcome and set up
-#   - clear screen
-#   - welcome message
-#   - initialize deck
-# 2 Start a round
-#   - display deck?
-#   - deal animation?
-#   - shuffle and remove cards from deck and add same to player hand
-#   - definitely display hands, player and dealer
-#   - display hand values
-#   - use chips (maybe), to control game length
-# 3 Ask player to hit or stay
-#   - if hit, deal player another card
-#       - display hand with new card
-#       - ask again to hit or stay, repeat until stay or bust
-#       - if bust, player loses
-#   - if stay, stop asking
-# 4 Dealer turn
-#   - if dealer hand < 17 hit, else stay
-#   - display hand with new card
-#   - if bust, player wins
-# 5 Compare hands
-#   - if player total is higher than dealer total, player wins
-#   - if hands are even and not bust, push (no win)
-#   - else, dealer wins
-# 6 Play again, but for how long? Should there be chips?
-#   Or just ask player after each hand if they want to continue?
-#
-# Include math for Ace to equal 11 or 1, depending on hand total.
-# Is 11 unless that makes hand bust 21, else 1
-
-# C
 """
-Get it working without chips, but keep the 'chips feature' in mind. 
+Twenty-One game with some bonus features. Instead of a best-of-5 feature,
+I planned to implement chips instead and elaborate on the spoopy/Twilight Zone
+"flavor". I may implement these things later, but got it to a state that I
+am happy with for now and feel that I need to keep moving forward. I deleted
+the code and comments relating to chips, but left the flavor/atmosphere alone.
 """
 import random
 import os
@@ -141,7 +41,7 @@ def welcome():
 *                                                                            *
 *                    ╭──────────────────────────────────╮                    *
 *                    │                                  │                    *
-*                    │         ★ Human Casino ★         │                    *
+*                    │         ★ Human Ca$ino ★         │                    *
 *                    │                                  │                    *
 *                    ╰──────────────────────────────────╯                    *
 *                                                                            *
@@ -207,6 +107,9 @@ def one_or_eleven(hand_value):
         return 11
 
 def hand_total(hand):
+    '''
+    Calculate and return the hand value.
+    '''
     hand_value = 0
     for card in hand:
         try:
@@ -243,11 +146,11 @@ def display_result(player_hand, dealer_hand, winner):
     if winner == 'player':
         prompt("Hmm ... looks like you beat me this time.")
     elif winner == 'dealer':
-        prompt("Dealer wins! Everything you've wagered is now mine!")
+        prompt("Dealer wins! Everything you've wagered is mine!")
     else:
         prompt("A tie. How dull! New hand! New hand, I say!")
 
-def color_card_face(value, suit):
+def color_card(suit):
     """Return the card face in the appropriate color based on suit."""
     if suit in RED_SUITS:
         color = ROW
@@ -264,31 +167,29 @@ def display_hand(hand):
     card_lower = "│     {}│"
     card_bottom = "╰──────╯"
 
-    face_down_top = "╭──────╮"
-    face_down_middle = f"{BOW}│▒▒▒▒▒▒│{RESET}"
-    face_down_bottom = "╰──────╯"
+    face_down_middle = f"{BOW}│▒▒▒▒▒▒│{RESET}" # Card back always BOW
 
     if len(hand) == 1:
-        hand.append(['▒', '▒']) # dealer hole card
+        hand.append(['▒', '▒']) # Dealer hole card
 
-    top_row = "   ".join([color_card_face(card[0], card[1]) +
+    top_row = "   ".join([color_card(card[1]) +
                            card_top + RESET for card in hand])
     upper_row = "   ".join([
-        color_card_face(card[0], card[1]) + card_upper.format(card[1]) +
+        color_card(card[1]) + card_upper.format(card[1]) +
           RESET if card[1] != '▒' else face_down_middle for card in hand
     ])
     middle_row = "   ".join([
-        color_card_face(card[0], card[1]) + card_middle_10.format(card[0]) +
+        color_card(card[1]) + card_middle_10.format(card[0]) +
           RESET if card[0] == '10'
-        else color_card_face(card[0], card[1]) +
+        else color_card(card[1]) +
           card_middle_other.format(card[0]) + RESET if card[0] != '▒'
         else face_down_middle for card in hand
     ])
     lower_row = "   ".join([
-        color_card_face(card[0], card[1]) + card_lower.format(card[1]) +
+        color_card(card[1]) + card_lower.format(card[1]) +
           RESET if card[1] != '▒' else face_down_middle for card in hand
     ])
-    bottom_row = "   ".join([color_card_face(card[0], card[1]) +
+    bottom_row = "   ".join([color_card(card[1]) +
                               card_bottom + RESET for card in hand])
 
     print(top_row)
@@ -391,7 +292,7 @@ def play_hand(deck):
         return
 
     dealer_turn(player_hand, dealer_hand, deck)
-    
+
     winner = who_won(player_hand, dealer_hand)
     display_result(player_hand, dealer_hand, winner)
 
